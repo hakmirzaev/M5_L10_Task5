ae79b8615fe0c27a952b543a2e8e6877ae16624f
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2022-11-30 19:07:04 +0500

    designing UI

--- M5_L10_Task5/ContentView.swift
+++ M5_L10_Task5/ContentView.swift
@@ -9,80 +9,39 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State var text = ""
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        VStack(spacing: 10){
+            Image("pdp")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.size.width, height: 200)
+                .mask(Text("PDP")
+                    .fontWeight(.black)
+                    .font(.system(size: 100))
+                    .frame(maxWidth: .infinity, alignment: .center))
+            Spacer()
+            Text("Submit your file here").font(.system(size: 20)).italic().bold()
+            HStack{
+                Image(systemName: "person")
+                TextField("Send a message", text: $text)
+                Button(action: {}, label: {
+                    Image(systemName: "paperclip")
+                })
+                Button(action: {}, label: {
+                    Image(systemName: "location").foregroundColor(.green)
+                })
+            }.padding()
+                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.gray, lineWidth: 1))
+        }.padding().frame(maxWidth: .infinity, alignment: .leading)
+            .background(LinearGradient(gradient: Gradient(colors: [.white,.blue,.white]), startPoint: .bottom, endPoint: .top))
+            
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
